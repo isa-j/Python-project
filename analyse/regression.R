e136c24eb801ee487e7480d8d03670cb8f3f789f
romainratajczyk <romain.ratajczyk@ens-lyon.fr>
2025-11-29 18:13:32 +0100

    code regression (summary montre clairement que la MCO n'est pas adaptée à notre variable quasi-binaire Y)

--- analyse/regression.R
+++ analyse/regression.R
@@ -1,40 +1,15 @@
-# nalyse de régression - Projet Delta Gentrification 
+# analyse de régression - Projet Delta Gentrification 
+library(haven)
+library(tidyverse)
+library(lmtest)
+library(sandwich)
+library(car)
+library(dplyr)
+library(estimatr)
 
+df <- read.csv("/Users/romain/Desktop/Projets DS/Python-project/analyse/data_pour_regression.csv")
 
-file_path <- "/Users/romain/Desktop/Projets DS/Python-project/analyse/data/data_deltas_pour_regression.csv"
+# 
+model_robust <- lm_robust(delta_elec ~ delta_pct_diplome + MED13 + delta_pop, data = df)
 
-
-df <- read.csv2(file_path)
-  
-# conversion colonne en numérique
-cols_to_numeric <- setdiff(names(df), "COM")
-df[cols_to_numeric] <- lapply(df[cols_to_numeric], as.numeric)
-  
-cat("Données chargées OK")
-cat(paste("Nombre total de communes :", nrow(df)))
-  
-
-# Analyse descriptive 
-cat("Summary, analyse descriptive")
-
-print(summary(df))
-
-
-# Regression linéaire MCO
-
-cat("Resultats MCO")
-
-
-# On explique Y (Score 2020) par les Deltas (X), les Stocks (X) et le Vote passé (X)
-formula_v2 <- Score_Gauche_Ecolo_2020 ~ Delta_Cadres + 
-                                       Delta_Diplomes + 
-                                       MED13 +  # On garde que 2013 pour éviter la multicollinéarité
-                                       log(P20_POP) + # On log-transforme la population
-                                       Score_Bloc_Gauche_2014
-
-
-model <- lm(formula, data = df)
-
-# Affichage du résumé complet du modèle
-
-print(summary(model))
+summary(model_robust)
