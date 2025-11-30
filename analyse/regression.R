d935c0b7497e397d91162a5669e426f49717e4f5
romainratajczyk <romain.ratajczyk@ens-lyon.fr>
2025-11-30 10:42:44 +0100

    changements infimes

--- analyse/regression.R
+++ analyse/regression.R
@@ -13,3 +13,20 @@ df <- read.csv("/Users/romain/Desktop/Projets DS/Python-project/analyse/data_pou
 model_robust <- lm_robust(delta_elec ~ delta_pct_diplome + MED13 + delta_pop, data = df)
 
 summary(model_robust)
+
+
+
+# REGRESSION LOGISTIQUE (EN COURS)
+
+#  Créer une variable binaire "Conquête"
+# 1 si la ville a basculé vers votre parti (delta = +1)
+# 0 sinon (si elle est restée stable ou a basculé dans l'autre sens)
+df$basculement_gauche <- ifelse(df$delta_elec == 1, 1, 0)
+
+# 2. Régression Logistique (GLM - Generalized Linear Model)
+# family = "binomial" dit à R : "C'est une proba, utilise une courbe en S (sigmoïde), pas une droite".
+model_logit <- glm(basculement_gauche ~ delta_pct_diplome + MED13 + delta_pop, 
+                   data = df, 
+                   family = "binomial")
+
+summary(model_logit)
